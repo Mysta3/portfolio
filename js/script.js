e68c1a29801e92eeaace8a5f56b0b7a3d29f17bc
Mysta3 <myshawnestallings@gmail.com>
2019-06-27 15:26:31 -0700

    fixed form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 
-
   /* Reference Messages Collection */ 
-  var db = firebase.firestore("https://contactform-105e8.firebaseio.com/");
+  //var db = firebase.firestore("https://contactform-105e8.firebaseio.com/");
+  var db = firebase.firestore();
 
 document.querySelector('#submit').addEventListener('click',
     function(){
-
         db.collection('newMessage').doc().set({
-            name : document.querySelector('#name').value,
-            email : document.querySelector('#email').value,
+            name    : document.querySelector('#name').value,
+            email   : document.querySelector('#email').value,
             message : document.querySelector('#message').value
         });
         console.log("submit successful")
@@ -24,14 +23,14 @@ function submitForm(e){
     e.preventDefault();
 
     //Get values
-    var name = document.getElementById(name).value; //getInputVal(name);
-    var email = document.getElementById(email).value; //getInputVal(email);
+    var name    = document.getElementById(name).value; //getInputVal(name);
+    var email   = document.getElementById(email).value; //getInputVal(email);
     var message = document.getElementById(message).value; //getInputVal(message);
 
 
     //call saveMessage funciton
     saveMessage(name, email, message);
-}
+} 
 
 /*
 //Function to get form values
