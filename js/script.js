2c5f511e40e31541ac85acac5434a5d7af4aef73
Myshawne Stallings <44353909+Mysta3@users.noreply.github.com>
2019-06-27 20:02:41 -0700

    Matching Master changes

--- js/script.js
+++ js/script.js
@@ -1,46 +1,41 @@
 
-
   /* Reference Messages Collection */ 
-  var db = firebase.firestore("https://contactform-105e8.firebaseio.com/");
+  var db = firebase.firestore();
 
 document.querySelector('#submit').addEventListener('click',
     function(){
-
         db.collection('newMessage').doc().set({
-            name : document.querySelector('#name').value,
-            email : document.querySelector('#email').value,
-            message : document.querySelector('#message').value
+            
+            name    : document.querySelector('#name').value,
+            email   : document.querySelector('#email').value,
+            message : document.querySelector('#message').value 
+            
         });
+        alert("Thanks for submitting! I look forward to connecting with you!");
         console.log("submit successful")
+
     }
 )
 
 
 
+/*
 //listen for Form submit
 document.getElementById("contactForm").addEventListener("submit", submitForm);
-
+*/
 function submitForm(e){
     e.preventDefault();
 
     //Get values
-    var name = document.getElementById(name).value; //getInputVal(name);
-    var email = document.getElementById(email).value; //getInputVal(email);
+    var name    = document.getElementById(name).value; //getInputVal(name);
+    var email   = document.getElementById(email).value; //getInputVal(email);
     var message = document.getElementById(message).value; //getInputVal(message);
 
 
     //call saveMessage funciton
     saveMessage(name, email, message);
-}
-
-/*
-//Function to get form values
-
-function getInputVal(id){
-    return document.getElementById(id).value;
-}
-
-*/
+   
+} 
 
 
 //Save Message to firebase messages table
@@ -52,6 +47,7 @@ function saveMessage(name, email, message){
         email: email,
         message: message
     });
+    
 }
 
 
